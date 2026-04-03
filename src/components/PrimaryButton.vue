23d48a2a32ac943906f7d9cac6da8cf110a7ed10
takahashiyb <takahashiyb89@gmail.com>
2026-04-04 00:37:14 +0800

    Create and style PrimaryButton component

---
+++ src/components/PrimaryButton.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts"></script>
+<template>
+  <button>Next Step</button>
+</template>
+<style scoped lang="scss">
+@use '@/assets/styles/main.scss' as v;
+@use '@/assets/styles/functions.scss' as f;
+
+button {
+  color: v.$white;
+  background-color: v.$blue-950;
+
+  padding-top: v.$spacing-0100;
+  padding-bottom: v.$spacing-0100;
+  padding-inline: v.$spacing-0200;
+
+  border-radius: 4px;
+  border: none;
+}
+
+button:hover {
+  background-color: v.$blue-700;
+}
+</style>
