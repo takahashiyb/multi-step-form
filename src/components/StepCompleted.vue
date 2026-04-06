48babc7d0180991c2d5115a618d66787634ae675
takahashiyb <takahashiyb89@gmail.com>
2026-04-06 18:19:53 +0800

    Created completed component

---
+++ src/components/StepCompleted.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts"></script>
+<template>
+  <div>
+    <img src="@/assets/images/icon-thank-you.svg" alt="icon for completed confirmation" />
+    <h2>Thank you!</h2>
+    <p class="subtext">
+      Thanks for confirming your subscription! We hope you have fun using our platform. If you ever
+      need support, please feel free to email us at support@loremgaming.com.
+    </p>
+  </div>
+</template>
+<style scoped lang="scss">
+div {
+  display: grid;
+  justify-items: center;
+  text-align: center;
+  align-content: center;
+}
+
+img {
+  padding-bottom: v.$spacing-0300;
+}
+</style>
